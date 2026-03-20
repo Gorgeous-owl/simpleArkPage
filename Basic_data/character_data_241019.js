3ef571e78e6a03922de20fc22ff079bc080e9c0d
mulberry <3013559425@qq.com>
2026-03-20 18:10:00 +0800

    first commit

---
+++ Basic_data/character_data_241019.js
@@ -0,0 +1,188 @@
+const  character_data_ = [
+    {
+        "name": "浊心斯卡蒂_skin2动态",
+        "data": "/7/78/立绘_浊心斯卡蒂_skin2.png",
+        "Dynamically": true,
+        "jsonUrl": "/skadi2_iteration.json",
+        "atlasUrl": "/skadi2_iteration.atlas",
+        "backgroundImage": "/spine_background/skadi2_iteration_bg.png"
+    },
+    {
+        "name": "凯尔希_skin1动态",
+        "data": "/0/0f/立绘_凯尔希_skin1.png",
+        "Dynamically": true,
+        "jsonUrl": "/kalts_boc.json",
+        "atlasUrl": "/kalts_boc.atlas",
+        "backgroundImage": "/spine_background/kalts_boc_bg.png"
+    },
+    {
+        "name": "夜莺_skin2",
+        "data": "/e/e5/立绘_夜莺_skin2.png",
+        "Dynamically": false,
+        "jsonUrl": 0,
+        "atlasUrl": 0,
+        "backgroundImage": 0
+    },
+    {
+        "name": "夜莺_skin2_动态",
+        "data": "/e/e5/立绘_夜莺_skin2.png",
+        "Dynamically": true,
+        "jsonUrl": "/cgbird_sightseer.json",
+        "atlasUrl": "/cgbird_sightseer.atlas",
+        "backgroundImage": "/spine_background/cgbird_sightseer_bg.png"
+    },
+    {
+        "name": "凯尔希_skin1",
+        "data": "/0/0f/立绘_凯尔希_skin1.png",
+        "Dynamically": false,
+        "jsonUrl": 0,
+        "atlasUrl": 0,
+        "backgroundImage": 0
+    },
+    {
+        "name": "立绘_塑心_1",
+        "data": "/2/28/立绘_塑心_1.png",
+        "Dynamically": false,
+        "jsonUrl": 0,
+        "atlasUrl": 0,
+        "backgroundImage": 0
+    },
+    {
+        "name": "立绘_塑心_2",
+        "data": "/b/b2/立绘_塑心_2.png",
+        "Dynamically": false,
+        "jsonUrl": 0,
+        "atlasUrl": 0,
+        "backgroundImage": 0
+    },
+    {
+        "name": "塑心_2动态",
+        "data": "/b/b2/立绘_塑心_2.png",
+        "Dynamically": true,
+        "jsonUrl": "/cello.json",
+        "atlasUrl": "/cello.atlas",
+        "backgroundImage": "/spine_background/cello_bg.png"
+    },
+    {
+        "name": "缪尔赛思_2",
+        "data": "/c/c7/立绘_缪尔赛思_2.png",
+        "Dynamically": false,
+        "jsonUrl": 0,
+        "atlasUrl": 0,
+        "backgroundImage": 0
+    },
+    {
+        "name": "缪尔赛思_2动态",
+        "data": "/c/c7/立绘_缪尔赛思_2.png",
+        "Dynamically": true,
+        "jsonUrl": "/mlyss.json",
+        "atlasUrl": "/mlyss.atlas",
+        "backgroundImage": "/spine_background/mlyss_bg.png"
+    },
+    {
+        "name": "史尔特尔_1",
+        "data": "/0/0c/立绘_史尔特尔_1.png",
+        "Dynamically": false,
+        "jsonUrl": 0,
+        "atlasUrl": 0,
+        "backgroundImage": 0
+    },
+    {
+        "name": "史尔特尔_skin2",
+        "data": "/6/69/立绘_史尔特尔_skin2.png",
+        "Dynamically": false,
+        "jsonUrl": 0,
+        "atlasUrl": 0,
+        "backgroundImage": 0
+    },
+    {
+        "name": "史尔特尔_skin2动态",
+        "data": "/6/69/立绘_史尔特尔_skin2.png",
+        "Dynamically": true,
+        "jsonUrl": "/surtr_summer.json",
+        "atlasUrl": "/surtr_summer.atlas",
+        "backgroundImage": "/spine_background/surtr_summer_bg.png"
+    },
+    {
+        "name": "迷迭香_1",
+        "data": "/5/59/立绘_迷迭香_1.png",
+        "Dynamically": false,
+        "jsonUrl": 0,
+        "atlasUrl": 0,
+        "backgroundImage": 0
+    },
+    {
+        "name": "迷迭香_skin1动态",
+        "data": "/b/ba/立绘_迷迭香_skin1.png",
+        "Dynamically": true,
+        "jsonUrl": "/rosmon_epoque.json",
+        "atlasUrl": "/rosmon_epoque.atlas",
+        "backgroundImage": "/spine_background/rosmon_epoque_bg.png"
+    },
+    {
+        "name": "浊心斯卡蒂_1",
+        "data": "/2/26/立绘_浊心斯卡蒂_1.png",
+        "Dynamically": false,
+        "jsonUrl": 0,
+        "atlasUrl": 0,
+        "backgroundImage": 0
+    },
+    {
+        "name": "假日威龙陈_skin1",
+        "data": "/f/f2/立绘_假日威龙陈_skin1.png",
+        "Dynamically": false,
+        "jsonUrl": 0,
+        "atlasUrl": 0,
+        "backgroundImage": 0
+    },
+    {
+        "name": "假日威龙陈_skin1动态",
+        "data": "/f/f2/立绘_假日威龙陈_skin1.png",
+        "Dynamically": true,
+        "jsonUrl": "/chen2_boc.json",
+        "atlasUrl": "/chen2_boc.atlas",
+        "backgroundImage": "/spine_background/chen2_boc_bg.png"
+    },
+    {
+        "name": "归溟幽灵鲨_2动态",
+        "data": "/1/16/立绘_归溟幽灵鲨_2.png",
+        "Dynamically": true,
+        "jsonUrl": "/ghost2.json",
+        "atlasUrl": "/ghost2.atlas",
+        "backgroundImage": "/spine_background/ghost2_bg.png"
+    },
+    {
+        "name": "焰影苇草_skin1动态",
+        "data": "/3/36/立绘_焰影苇草_skin1.png",
+        "Dynamically": true,
+        "jsonUrl": "/reed2_epoque.json",
+        "atlasUrl": "/reed2_epoque.atlas",
+        "backgroundImage": "/spine_background/reed2_epoque_bg.png"
+    },
+    {
+        "name": "伊内丝_skin1动态",
+        "data": "/2/2c/立绘_伊内丝_skin1.png",
+        "Dynamically": true,
+        "jsonUrl": "/ines_boc.json",
+        "atlasUrl": "/ines_boc.atlas",
+        "backgroundImage": "/spine_background/ines_boc_bg.png"
+    },
+    {
+        "name": "缄默德克萨斯_skin1动态",
+        "data": "/b/b9/立绘_缄默德克萨斯_skin1.png",
+        "Dynamically": true,
+        "jsonUrl": "/texas2_iteration.json",
+        "atlasUrl": "/texas2_iteration.atlas",
+        "backgroundImage": "/spine_background/texas2_iteration_bg.png"
+    },
+    {
+        "name": "鸿雪_skin1动态",
+        "data": "/9/9d/立绘_鸿雪_skin1.png",
+        "Dynamically": true,
+        "jsonUrl": "/bgsnow_wild.json",
+        "atlasUrl": "/bgsnow_wild.atlas",
+        "backgroundImage": "/spine_background/bgsnow_wild_bg.png"
+    },
+]
+
+export default character_data_
